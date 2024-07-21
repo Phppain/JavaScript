5e8c37f14d368fdbcebe8b9c37add85924277d0c
Phppain <119727060+Phppain@users.noreply.github.com>
2024-07-21 19:06:58 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+// Home Work
+
+// //Task 1
+// let user_name = prompt("Enter your name, please");
+// alert("Hello, " + user_name);
+
+// //Task2
+// const current_year = 2024;
+// let year_of_born = +prompt("Enter your year of born");
+// alert("Your age: " + String(current_year - year_of_born));
+
+// //Task3
+// let square_side = +prompt("Enter a square's side");
+// alert("Square's P = " + String(square_side * 4));
+
+// //Task4
+// const number_p = 3.14;
+// let radius = +prompt("Enter a radius");
+// alert("S = " + String(number_p * radius ** 2));
+
+// //Task5
+// let range = +prompt("Enter a range(km)");
+// let time = +prompt("Enter a time(h)");
+// alert(
+//     "The speed at which you have to move to get there on time: " +
+//         String(range / time)
+// );
+
+// //Task6
+// const euro_exchange_rate = 0.92;
+// let dollars = +prompt("Enter a dollars");
+// alert("Euro = " + String(euro_exchange_rate * dollars));
+
+// //Task7
+// let gb = +prompt("Enter a gb");
+// alert("= " + String((gb * 1024) / 820));
+
+//Task8
+// let money = +prompt("Enter the amount of money in your wallet: ");
+// let price = +prompt("Enter the price of one chocolate: ");
+// alert(
+//     `You can buy ${Math.floor(money / price)} chocolates. Your change will be ${
+//         money % price
+//     } money.`
+// );
+
+//Task9
+// let number = +prompt("Enter a three-digit number: ");
+// let hundreds = Math.floor(number / 100);
+// let tens = Math.floor((number % 100) / 10);
+// let ones = number % 10;
+// let reversedNumber = ones * 100 + tens * 10 + hundreds;
+// alert(`Your number reversed is: ${reversedNumber}`);
